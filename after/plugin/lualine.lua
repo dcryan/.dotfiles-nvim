70febc7e95553c97c796121cbf0419124a04d792
Daniel Ryan <d@nielryan.com>
2023-01-05 16:04:32 -0700

    feat: better theme for lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,7 +1,7 @@
 require('lualine').setup {
   options = {
-    theme  = 'material' ,
-    section_separators = { left = '' , right = '' },
-    component_separators = { left = '' , right = '' }
+    theme                = 'material',
+    section_separators   = { left = '', right = '' },
+    component_separators = { left = '', right = '' }
   }
 }
