c034fb558a244b064bd33a181635185961371b61
Daniel Ryan <d@nielryan.com>
2023-01-05 16:03:17 -0700

    feat: not using ale right now

--- lua/dryan/packer.lua
+++ lua/dryan/packer.lua
@@ -30,7 +30,7 @@ return require('packer').startup(function(use)
 
   use "lewis6991/gitsigns.nvim"
   use 'bronson/vim-trailing-whitespace'
-  use 'dense-analysis/ale'
+  -- use 'dense-analysis/ale'
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true },
