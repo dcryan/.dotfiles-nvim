ec6e6c7648df5a713061b4297db8d0756f93c208
Daniel Ryan <d@nielryan.com>
2025-03-01 15:44:04 +0100

    chore: better diagnostic windows

--- lua/configs/set.lua
+++ lua/configs/set.lua
@@ -43,7 +43,24 @@ vim.opt.foldlevel = 99
 vim.opt.foldcolumn = "1"
 
 -- Assign signs to Diagnostics
-vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+vim.diagnostic.config({
+	underline = true,
+	virtual_text = false,
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "",
+			[vim.diagnostic.severity.WARN] = "",
+			[vim.diagnostic.severity.INFO] = "",
+			[vim.diagnostic.severity.HINT] = "",
+		},
+	},
+	update_in_insert = false,
+
+	severity_sort = true,
+	float = {
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+	},
+})
