81b0153e9fb7fb7c7954ff54ca9f57ac9252d76e
Daniel Ryan <d@nielryan.com>
2025-03-01 15:38:56 +0100

    chore(tokyonight): updating

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,10 +1,17 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      transparent = true,
-    },
-  }
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			transparent = true,
+			dim_inactive = true,
+			on_highlights = function(hl, c)
+				-- Make borders slightly more legible VertSplit didn't seem to do anything
+				hl.WinSeparator = {
+					fg = c.cyan,
+				}
+			end,
+		},
+	},
 }
