afc48f2830cea63b158b6ac62ef1c7b3f2d5f299
Daniel Ryan <d@nielryan.com>
2025-03-01 15:40:37 +0100

    feat: removing lsp-zero in favor of pure mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,147 @@
 return {
-	"williamboman/mason.nvim",
-	build = function()
-		pcall(vim.cmd, "MasonUpdate")
-	end,
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"neovim/nvim-lspconfig",
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+
+		opts = {
+			ensure_installed = {
+				"ts_ls",
+				"pyright",
+				"prismals",
+				"lua_ls",
+				"jsonls",
+				"tailwindcss",
+				"tflint",
+			},
+			automatic_installation = true,
+		},
+
+		config = function(_, opts)
+			local mason_lspconfig = require("mason-lspconfig")
+			mason_lspconfig.setup(opts)
+
+			local lspconfig = require("lspconfig")
+			local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+			-- used to enable autocompletion (assign to every lsp server config)
+			local capabilities = cmp_nvim_lsp.default_capabilities()
+
+			mason_lspconfig.setup_handlers({
+				-- default handler for installed servers
+				function(server_name)
+					lspconfig[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
+				["lua_ls"] = function()
+					lspconfig.lua_ls.setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								runtime = {
+									version = "LuaJIT",
+								},
+								diagnostics = {
+									globals = { "vim" },
+								},
+								workspace = {
+									library = { vim.env.VIMRUNTIME },
+								},
+							},
+						},
+					})
+				end,
+			})
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					-- Buffer local mappings.
+					-- See `:help vim.lsp.*` for documentation on any of the below functions
+					local opts = { buffer = ev.buf, silent = true }
+
+					opts.desc = "Show hover information"
+					vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+
+					opts.desc = "Go to definition"
+					vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+
+					opts.desc = "Go to declaration"
+					vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+
+					opts.desc = "Go to implementation"
+					vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+
+					opts.desc = "Go to type definition"
+					vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+
+					opts.desc = "Show references"
+					vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+
+					opts.desc = "Show signature help"
+					vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+
+					opts.desc = "Show diagnostics"
+					vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+
+					opts.desc = "Rename symbol"
+					vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+
+					opts.desc = "Format document"
+					vim.keymap.set("n", "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<CR>", opts)
+
+					opts.desc = "Format document"
+					vim.keymap.set("x", "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<CR>", opts)
+
+					opts.desc = "Show code actions"
+					vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+				end,
+			})
+		end,
+	},
+
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
 }
